6ac4ac43b3d8629a4c32b48ec07862cdfd386a04
mateusz.fraszczynski <mateusz.mfc@wp.pl>
2017-09-18 14:41:09 +0200

    Tab fix

--- include/UI/MainWindow.h
+++ include/UI/MainWindow.h
@@ -17,8 +17,8 @@
 #define MAIN_WIN_SIZE_Y 				500
 #define MAIN_DRAW_TAB_X					200
 #define MAIN_DRAW_TAB_Y				 	30
-#define MAIN_TEXT_OFFSET_X			230
-#define MAIN_TEXT_OFFSET_Y			38
+#define MAIN_TEXT_OFFSET_X				230
+#define MAIN_TEXT_OFFSET_Y				38
 #define MAIN_TABLE_COLUMNS			 	5
 #define MAIN_COLUMN_WIDTH 				100
 #define MAIN_COLUMN_HIGH				30
